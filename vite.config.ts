4d0eab8ffecaa777d6a99e94221757ed737538d3
escritoriorenatocosta-debug <escritoriorenatocosta@gmail.com>
2026-06-02 09:55:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
+            base: '/BORABRASILCEPE/'
     },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
